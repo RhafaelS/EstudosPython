0715545a7decf4d0178ba9311aef933a08ebeaf6
Rhafael <rhafael.cesar@outlook.com>
2018-04-27 23:05:55 -0300

    atualizando

--- alien.py
+++ alien.py
@@ -1,6 +1,12 @@
 alien_color = 'green'
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {}
 
-if alien_color == 'green':
-    print("Você ganhou 5 pontos!")
-else:
-    print("Você ganhou 10 pontos!")
+alien_1['color'] = 'blue'
+alien_1['points'] = 10
+alien_1['type'] = 'mega alien'
+alien_1['hp'] = 10
+alien_1['shield'] = 5
+
+for property in alien_1:
+    print(alien_1[property])
