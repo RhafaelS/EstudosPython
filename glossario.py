07ca2dee6a7e76ff1ce24f6d7de8216d0e5b4f37
Rhafael <rhafael.cesar@outlook.com>
2018-04-28 10:42:02 -0300

    glossario com dicionário

---
+++ glossario.py
@@ -0,0 +1,9 @@
+glossario = {
+    'del': 'Apaga o registro em questão.',
+    'append': 'Adiciona um elemento no fim de uma lista.',
+    'if': 'Comando para testar uma determinada condição.',
+    'for': 'Comando utilizado para realizar determinadas repetições até que a condição seja atendida.'
+}
+
+for comando in glossario:
+    print(comando + ": " + glossario[comando])
