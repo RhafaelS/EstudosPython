34938462495ba819e1b016024df4460c82f32ba1
Rhafael <rhafael.cesar@outlook.com>
2018-05-05 10:04:25 -0300

    whiles.py

--- whiles.py
+++ whiles.py
@@ -1,2 +1,6 @@
-message = input("Fale algo que eu vou repetir: ")
-print(message)
+numero = input("Entre com um número e eu irei decrementá-lo até 0: ")
+numero = int(numero)
+
+while numero >= 0:
+    print(numero)
+    numero = numero - 1
