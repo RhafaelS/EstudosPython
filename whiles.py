b685933e48039a3c6b3430d9640e54983cb7fa32
Rhafael <rhafael.cesar@outlook.com>
2018-05-06 12:07:39 -0300

    whiles.py

--- whiles.py
+++ whiles.py
@@ -1,6 +1,21 @@
-numero = input("Entre com um número e eu irei decrementá-lo até 0: ")
-numero = int(numero)
+responses = {}
 
-while numero >= 0:
-    print(numero)
-    numero = numero - 1
+polling_active = True
+
+def show_data(name, mountain):
+    print(name + " would like to climb " + response + ".")
+
+while polling_active:
+    name = input("\nWhats is your name?")
+    response = input("Which mountain would you like to climb someday?")
+
+    responses[name] = response
+
+    repeat = input("Would you like to let another person respond? (yes/no) ")
+    if repeat == 'no':
+        polling_active = False
+
+
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+    show_data(name, response)
